8c8eea359b80cc88d3a8f44847421f986232c534
Zeeshan Raza <zeeshanrahman72@gmail.com>
2026-02-17 23:13:23 +0530

    removed built in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -28,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <h1>hello world</h1>
+      <h1>Zeeshan Raza</h1>
     </>
   )
 }
